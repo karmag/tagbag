7b61ff93d70789d218ed3ad48451ec4ae5334cf6
Karl Magnusson <karl.k.magnusson@gmail.com>
2025-10-26 19:35:36 +0100

    padding for components

--- src/Tagbag.Gui/Root.cs
+++ src/Tagbag.Gui/Root.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Tagbag.Gui;
@@ -15,27 +17,49 @@ public class Root : Form
         var grid = MakeGridView(data);
         grid.Dock = DockStyle.Fill;
         Controls.Add(grid);
+
+        Text = "Tagbag";
     }
 
     private Control MakeGridView(Data data)
     {
+        var pad = 5;
+
         var panel = new Control();
 
         data.ImageGrid.Dock = DockStyle.Fill;
+        data.ImageGrid.Padding = new Padding(pad);
         panel.Controls.Add(data.ImageGrid);
 
         var split = new Splitter();
+        split.Width = 5;
         panel.Controls.Add(split);
 
-        data.TagTable.Dock = DockStyle.Left;
-        panel.Controls.Add(data.TagTable);
+        var ttPanel = new Panel();
+        ttPanel.Padding = new Padding(pad);
+        ttPanel.Dock = DockStyle.Left;
+        data.TagTable.Dock = DockStyle.Fill;
+        ttPanel.Controls.Add(data.TagTable);
+        panel.Controls.Add(ttPanel);
 
         data.TagTable.SetEntry(data.EntryCollection.Get(0));
 
+        var inputPanel = new Panel();
+        inputPanel.BackColor = Color.DarkGray;
         var input = new TextBox();
-        input.Dock = DockStyle.Bottom;
-        panel.Controls.Add(input);
-        
+        input.Top = pad;
+        input.Font = new Font("Courier New", 16);
+        inputPanel.ClientSizeChanged += (_, _) =>
+        {
+            input.Width = Math.Max(300, inputPanel.Width / 2);
+            input.Left = (inputPanel.Width - input.Width) / 2;
+        };
+        inputPanel.Dock = DockStyle.Bottom;
+        inputPanel.Padding = new Padding(pad);
+        inputPanel.Controls.Add(input);
+        inputPanel.Height = input.Height + pad*2;
+        panel.Controls.Add(inputPanel);
+
         return panel;
     }
 }
