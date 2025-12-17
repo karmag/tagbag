0433a2993d78e9d156467af27260674fa78ffde6
Karl Magnusson <karl.k.magnusson@gmail.com>
2025-12-17 20:47:25 +0100

    unload low use images

--- src/Tagbag.Gui/ImageCache.cs
+++ src/Tagbag.Gui/ImageCache.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Drawing;
 using System.IO;
@@ -9,29 +10,45 @@ namespace Tagbag.Gui;
 
 public class ImageCache
 {
+    private decimal _ThumbnailWidth;
+    private decimal _ThumbnailHeight;
+    private int _MaxImages;
+    private int _MaxThumbnails;
+
     private Tagbag.Core.Tagbag? _Tagbag;
     private ConcurrentDictionary<Guid, Task<Bitmap?>> _ImageCache;
     private ConcurrentDictionary<Guid, Task<Bitmap?>> _ThumbnailCache;
     private ConcurrentStack<Task<Bitmap?>> _TaskStack;
     private ConcurrentStack<Task<Bitmap?>> _TaskHighPrio;
+    private RecencyQueue<Guid> _RecentImages;
+    private RecencyQueue<Guid> _RecentThumbnails;
+
     private Thread _Worker;
     private bool _Running;
 
-    private decimal _ThumbnailWidth;
-    private decimal _ThumbnailHeight;
-
     public ImageCache(EventHub eventHub)
     {
+        _ThumbnailWidth = 300;
+        _ThumbnailHeight = 300;
+        _MaxImages = 10;
+        _MaxThumbnails = 100;
+
         _Tagbag = null;
         _ImageCache = new ConcurrentDictionary<Guid, Task<Bitmap?>>();
         _ThumbnailCache = new ConcurrentDictionary<Guid, Task<Bitmap?>>();
         _TaskStack = new ConcurrentStack<Task<Bitmap?>>();
         _TaskHighPrio = new ConcurrentStack<Task<Bitmap?>>();
+        _RecentImages = new RecencyQueue<Guid>(_MaxImages, (guid) => {
+            Task<Bitmap?>? task;
+            _ImageCache.TryRemove(guid, out task);
+        });
+        _RecentThumbnails = new RecencyQueue<Guid>(_MaxThumbnails, (guid) => {
+            Task<Bitmap?>? task;
+            _ThumbnailCache.TryRemove(guid, out task);
+        });
+
         _Worker = new Thread(WorkerFunction);
         _Running = true;
-
-        _ThumbnailWidth = 300;
-        _ThumbnailHeight = 300;
 
         eventHub.Shutdown += (_) => { _Running = false; };
 
@@ -52,7 +69,7 @@ public class ImageCache
     // queue.
     public Task<Bitmap?> GetImage(Guid id, bool prio = false)
     {
-        var task = GetOrLoad(id, _ImageCache);
+        var task = GetOrLoad(id, _ImageCache, _RecentImages);
         if (prio && !task.IsCompleted)
             _TaskHighPrio.Push(task);
         return task.ContinueWith(CopyBitmap).Unwrap();
@@ -62,7 +79,7 @@ public class ImageCache
     // thumbnail.
     public Task<Bitmap?> GetThumbnail(Guid id, bool prio = false)
     {
-        var task = GetOrLoad(id, _ThumbnailCache, ResizeAsThumbnail);
+        var task = GetOrLoad(id, _ThumbnailCache, _RecentThumbnails, ResizeAsThumbnail);
         if (prio && !task.IsCompleted)
             _TaskHighPrio.Push(task);
         return task.ContinueWith(CopyBitmap).Unwrap();
@@ -71,6 +88,7 @@ public class ImageCache
     private Task<Bitmap?> GetOrLoad(
         Guid id,
         ConcurrentDictionary<Guid, Task<Bitmap?>> cache,
+        RecencyQueue<Guid> recent,
         Func<Bitmap, Bitmap>? transform = null)
     {
         Task<Bitmap?>? existing;
@@ -94,6 +112,7 @@ public class ImageCache
         if (cache.TryAdd(id, task))
         {
             _TaskStack.Push(task);
+            recent.AddAndPop(id);
             return task;
         }
         else if (cache.TryGetValue(id, out existing))
@@ -158,6 +177,78 @@ public class ImageCache
             {
                 Thread.Sleep(250);
             }
+        }
+    }
+
+    private class RecencyQueue<T> where T : IComparable<T>
+    {
+        private int _Max;
+        private Action<T>? _CleanupFunction;
+
+        private HashSet<T> _Existence;
+        private List<T?> _Ordered;
+        private int _Index;
+
+        // When the queue becomes filled AddAndPop will call the
+        // cleanup function with the item being removed from the
+        // queue.
+        public RecencyQueue(int max, Action<T>? cleanupFunction = null)
+        {
+            _Max = max;
+            _CleanupFunction = cleanupFunction;
+
+            _Existence = new HashSet<T>();
+            _Ordered = new List<T?>();
+            _Index = 0;
+
+            for (int i = 0; i < max; i++)
+                _Ordered.Add(default(T));
+        }
+
+        // Adds the given item to the Queue. Returns the item that was
+        // pushed out, if any.
+        public T? AddAndPop(T t)
+        {
+            lock (this)
+            {
+                if (_Existence.Contains(t))
+                {
+                    for (int i = 0; i < _Max; i++)
+                    {
+                        if (t.Equals(_Ordered[i]))
+                        {
+                            // TODO: just dumping the element isn't
+                            // enough, need to squash the remaining
+                            // elements together as well
+                            _Ordered[i] = default(T);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    _Existence.Add(t);
+                }
+
+                var old = _Ordered[_Index % _Max];
+                _Ordered[_Index % _Max] = t;
+                _Index++;
+                if (old is T oldT)
+                {
+                    _Existence.Remove(oldT);
+                    _CleanupFunction?.Invoke(oldT);
+                }
+
+                return old;
+            }
+        }
+
+        public void Clear()
+        {
+            _Existence.Clear();
+            for (int i = 0; i < _Max; i++)
+                _Ordered[i] = default(T);
+            _Index = 0;
         }
     }
 }
